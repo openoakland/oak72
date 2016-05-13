fc84815702338811750f8a4bdc0cb6c036d3697c
Arena <arena@visualarena.com>
2016-05-13 12:33:38 -0700

    changed the title of the site
    Now the title comes from a yml file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "#{@city.abbreviation}72"
+    base_title = t(:site_title)
     if page_title.empty?
       base_title
     else
@@ -12,5 +12,5 @@ module ApplicationHelper
   def city_residents
     @city.moniker.blank? ? "#{@city.name} residents" : @city.moniker
   end
-  
+
 end
