1b2c44a9b26a6dd99a0d12492e7ddb4e357a2ce2
Sameer Siruguri <siruguri@gmail.com>
2016-05-19 11:19:35 -0700

    Two versions of story page

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,9 +3,22 @@ class StoriesController < ApplicationController
   after_filter :static_content
 
   def index
+    collect_stories
+  end
+
+  def show
+    collect_stories
+    if params[:id] == 'v2'
+      render :stories_2
+    else
+      redirect_to :index
+    end
+  end
+
+  private
+  def collect_stories
     all_stories = EmergencyStory.order("index,id ASC").all
     @selected_story = all_stories.select {|s| s.selected}.first
     @stories = all_stories.collect.select {|s| !s.selected}
-  end
-
+  end    
 end
